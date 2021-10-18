9045e4e5b9c1c7f87e9e5eb46f94693513754345
SE <90259048+5ahexn@users.noreply.github.com>
2021-10-18 18:28:25 +0900

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    // navigation bar toggle
+    $('#navbar-toggler').click(function() {
+        $('.navbar-collapse').slideToggle(44);
+    })
+})
+
+//navbar bg change on scroll
+$(window).scroll(function() {
+    let pos = $(window).scrollTop();
+    if (pos > 100) {
+        $('.navbar').addClass('cng-navbar');
+    } else {
+        $('.navbar').removeClass('cng-navbar');
+    }
+})
