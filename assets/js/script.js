f2002201e61ff3af3c6d604abb82bce38cc5d3c2
5ahexn <5ahexn@gmail.com>
2021-10-21 01:02:12 +0900

    Add & Edit
    html, css, js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,30 @@
-$(document).ready(function() {
-    // navigation bar toggle
-    $('#navbar-toggler').click(function() {
-        $('.navbar-collapse').slideToggle(44);
-    })
-})
+$(document).ready(function () {
 
-//navbar bg change on scroll
-$(window).scroll(function() {
-    let pos = $(window).scrollTop();
-    if (pos > 100) {
-        $('.navbar').addClass('cng-navbar');
-    } else {
-        $('.navbar').removeClass('cng-navbar');
-    }
-})
+    // Navigation bar toggle
+    $('#navbar-toggler').click(function () {
+        $('.navbar-collapse').slideToggle(400);
+    })
+
+    // Navbar bg change on scroll
+    $(window).scroll(function () {
+        let pos = $(window).scrollTop();
+        if (pos > 100) {
+            $('.navbar').addClass('cng-navbar');
+        } else {
+            $('.navbar').removeClass('cng-navbar');
+        }
+    });
+
+    // Sample video popup
+    $(document).ready(function () {
+        $('.popup-youtube').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+
+            fixedContentPos: false
+        });
+    });
+});
