17b526dc146e91b500c52a21a56d3cb98f552e33
5ahexn <5ahexn@gmail.com>
2021-10-22 03:52:02 +0900

    Add
    CSS and JavaScript file
    Html

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,4 +27,41 @@ $(document).ready(function () {
             fixedContentPos: false
         });
     });
+
+    // Team carousel
+    $('.team .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        dots: true,
+        nav: false,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items: 1
+            },
+            600:{
+                items: 2
+            },
+            1000:{
+                items: 3
+            }
+        }
+    })
+
+    // FAQ accordion
+    $('.faq-head').each(function(){
+        $(this).click(function(){
+            $(this).next().toggleClass('show-faq-content');
+            let icon = $(this).children('span').children("i").attr('class');
+
+            if(icon == "fas fa-plus"){
+                $(this).children('span').html('<i class = "fas fa-minus"></i>');
+            } else {
+                $(this).children('span').html('<i class = "fas fa-plus"></i>');
+            }
+        });
+    });
+
+    
 });
